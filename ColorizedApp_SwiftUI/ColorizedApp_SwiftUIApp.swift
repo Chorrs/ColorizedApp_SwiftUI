d43f39666558f83960c56f182acc93db420a04e0
Ekaterina Pecherskaya <Pechorskaya.K8@gmail.com>
2023-12-17 15:49:52 +0200

    Initial Commit

---
+++ ColorizedApp_SwiftUI/ColorizedApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_SwiftUIApp.swift
+//  ColorizedApp_SwiftUI
+//
+//  Created by Chorrs on 17.12.23.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
